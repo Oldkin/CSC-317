17e996e0b438d8f1a22773d5e7e992b63465acf9
Akash Surti <asurti17@csl16-l.cornellcollege.edu>
2016-10-20 20:32:51 -0500

    License

--- MultithreaderServer.java
+++ MultithreaderServer.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author rconrardy17
+ * @author rconrardy17, asurti17
  */
 import java.io.* ;
 import java.net.* ;
